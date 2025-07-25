b2ab93851f9755f256f713a8eab97dd7d07c5cd5
Micheal Hany <micheal.hany@dsquares.com>
2025-07-26 02:45:50 +0300

    delete unused file, fix package.swift files

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -1,4 +1,7 @@
-// swift-tools-version: 5.8
+// swift-tools-version:5.8
+//
+//  Package@swift-5.8.swift
+//
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,29 +12,17 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Refresher",
             targets: ["Refresher"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "1.0.0"),
-        .package(url: "https://github.com/gh123man/SwiftUI-RenderLock", from: "1.0.2"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Refresher",
-            dependencies: [
-                .product(
-                    name: "SwiftUIIntrospect",
-                    package: "SwiftUI-Introspect"),
-                .product(
-                    name: "RenderLock",
-                    package: "SwiftUI-RenderLock")
-            ]
-        ),
-    ]
+            name: "Refresher", path: "Sources"),
+        
+    ],
+    swiftLanguageVersions: [.v5]
 )
